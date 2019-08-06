bb3442a215e186e03e7fc10995e581c64f79c30b
Ashraf-Omar <ashrafomar6@gmail.com>
2019-08-06 15:48:54 +0200

    Prevent cloning of demi's repos and added shallow cloning

--- CloneAllProjects.py
+++ CloneAllProjects.py
@@ -4,14 +4,12 @@ Created on Mon Aug  5 18:35:30 2019
 
 @author: Ashraf
 """
-import requests, json, os, getpass, shutil
+import requests, json, os, getpass
+demis = ['2018-project-560895-Linda','2018-project-571133-Kyle','2018-project-714227-Kayla','2018-project-717931-Kishan','2018-project-816424-Conrad','2018-project-837873-Malu']
 
 def main():
-    user, pswd, access_token = getInput()
-    if access_token:
-        cloneAllProjects(user, pswd, access_token)
-    else:
-        cloneAllProjects(user, pswd)
+    user, pswd = getInput()
+    cloneAllProjects(user, pswd)
 
 def cloneAllProjects(user, pswd, access_token = '146da8c3ad930d598318a94068fe158c716f6496', directory = os.getcwd() + os.sep + r'Repositories'):
     filename = os.getcwd() + os.sep + 'Repos.json'
@@ -37,9 +35,10 @@ def cloneAllProjects(user, pswd, access_token = '146da8c3ad930d598318a94068fe158
         os.makedirs(directory)
     os.chdir(directory)    
     for repo in json_string:
-        name = repo['full_name']
-        print('Cloning repository: ' + name)
-        os.system('git clone ' + repo['ssh_url'])
+        name = repo['name']
+        if name not in demis:
+            print('Cloning repository: ' + name)
+            os.system('git clone --depth 1 ' + repo['ssh_url'])
     print('Done.')
 
 def getInput():
@@ -49,18 +48,8 @@ def getInput():
     while True:
         pswd = getpass.getpass('Enter GitHub password: ')
         break
-    while True:
-        access_token = input('(OPTIONAL, leave blank to skip) Enter GitHub user oAuth token: \n')
-        break
-    return user, pswd , access_token
+    return user, pswd
 
 if __name__ == '__main__':
     main()
-    print('Removing demi repos...')
-    demis = ['2018-project-560895-Linda','2018-project-571133-Kyle','2018-project-714227-Kayla','2018-project-717931-Kishan','2018-project-816424-Conrad','2018-project-837873-Malu']
-    for folder in demis:
-        try:
-            shutil.rmtree(os.getcwd()+os.sep+folder)
-        except OSError as e:
-            print ("Error: %s - %s." % (e.filename, e.strerror))
     k = input('Press enter to exit...')
