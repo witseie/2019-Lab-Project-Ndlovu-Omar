6addcb48051eed5db0c88d85c05e4c681d8574c9
Sanele <sellout140@gmail.com>
2019-08-11 21:53:09 +0200

    Updated Gui to choose which project to analyse

--- gui.py
+++ gui.py
@@ -7,12 +7,17 @@ Created on Tue Aug  6 22:19:47 2019
 import os
 import tkinter as tk
 import CloneAllProjects
+import DevProject
 from tkinter import* 
 
 def userDetails():
     username = entry_1.get()
     password = entry_2.get()
     CloneAllProjects.main(username,password)
+    
+def prints():
+    clicked = lbox.get(lbox.curselection())
+    DevProject.AnalyseProject(clicked)
 
 my_window = Tk()
 frame = Frame( my_window )
@@ -25,7 +30,7 @@ entry_1 = Entry(my_window)
 label_2 = Label(my_window, text = "Enter your password: ")
 entry_2 = Entry(my_window, show='*')
 button_1 = Button(my_window,text = "Clone Repository",command=userDetails)
-button_2 = Button(my_window,text = "Analyse")
+button_2 = Button(my_window,text = "Analyse",command=prints)
 
 label_1.grid(row=0,column=0)
 entry_1.grid(row=0,column=1)
@@ -35,13 +40,16 @@ button_1.grid(row=1,column=0)
 button_2.grid(row=1,column=1)
 
 flist = os.listdir(r'C:\Users\user\Desktop\labproject\Lab Project\Repositories')
- 
-lbox = tk.Listbox(my_window)
+
+lbox = Listbox(my_window,width = 30, height = 15, selectmode = SINGLE)
 lbox.grid(row=4,column=0)
  
 # THE ITEMS INSERTED WITH A LOOP
 for item in flist:
     lbox.insert(tk.END, item)
- 
+    
+
+
+
 my_window.mainloop()
     
