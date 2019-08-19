0edf9adb5adfd5c6c55f01aca4acbdd461860de4
Ashraf-Omar <ashrafomar6@gmail.com>
2019-08-19 14:49:29 +0200

    Added testing for ABC's and minor bug fixing
    Solved bug that caused some classes not to be parsed.

--- Inheritance.py
+++ Inheritance.py
@@ -33,15 +33,14 @@ def functionGrammer():
 def classGrammer():
     cppName = Word(alphas+':_', alphanums+':_')
 #    constKeyword = CaselessKeyword("const")
-    class_keyword = Suppress(CaselessKeyword('class')) #+ ~PrecededBy(CaselessKeyword('enum'))
+    class_keyword = Suppress(CaselessKeyword('class'))
     class_name =  cppName
-    class_body = nestedExpr('{', '};', ignoreExpr=(cppStyleComment.suppress()))    
+    class_body = Group(nestedExpr('{', '}') + Char(';'))
     parent_class = Group(Suppress(CaselessKeyword('public')) + class_name)
     inherits =  Suppress(Char(':')) + delimitedList(parent_class)
     scoped_enum = Group(CaselessKeyword('enum') + class_keyword + class_name + class_body)
     
-    Grammer = (class_keyword + class_name("name") + Optional(inherits)("parents") + originalTextFor(class_body)("body"))
-#    Grammer = originalTextFor(Grammer)
+    Grammer = (class_keyword + class_name("name") + Optional(inherits)("parents") + (originalTextFor(class_body)("body")))
     Grammer = Grammer.ignore(scoped_enum)
     return Grammer
 
@@ -51,25 +50,32 @@ grammer = classGrammer()
 project = DevProject.DevProject(fileDirectory)
 classList = []
 inheritance = 0
+abc = 0
+c_function, virtual_function = functionGrammer()
+
 for file in project.getFiles():
-#    if file.getName().endswith('.h'):
-    cont = file.getContents()
-    classes = grammer.searchString(removeComments(cont))
-    for cppClass in classes:
-        classList.append(cppClass.asList())
-        if cppClass.parents:
-            inheritance += 1
+    if file.getName().endswith('.h'):
+#        print(file.getName())
+        cont = removeComments(file.getContents())
+        classes = grammer.searchString(cont)
+        for cppClass in classes:
+#            print(cppClass.name)
+            classList.append(cppClass.asList())
+            if cppClass.parents:
+                inheritance += 1
+            source_code = cppClass.body
+            functions = c_function.searchString(source_code).asList()
+            abstr_functions = virtual_function.searchString(source_code).asList()
+            if len(functions)==len(abstr_functions):
+                print(cppClass.name + " is an ABC")
+                abc += 1
+#            else:
+#                print("It's not an ABC")
 print('Number of classes in the project: ' + str(len(classList)))
 print('Number of classes using inheritance: ' + str(inheritance))
+print('Number of abstract base classes: ' + str(abc))
 
-#c_function, virtual_function = functionGrammer()
-#source_code = file.getContents()
-#a = c_function.searchString(source_code).asList()
-#b=virtual_function.searchString(source_code).asList()
-#if len(a)==len(b):
-#    print("It's an ABC")
-#else:
-#    print("It's not an ABC")
+
         
 #for func in a:
 #    print("%(name)s (%(type)s) args: %(args)s" % func)
