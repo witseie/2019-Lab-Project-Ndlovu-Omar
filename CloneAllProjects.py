407d1a7d41d61063b3cb2b969593059b5d1ed7d3
Ashraf-Omar <ashrafomar6@gmail.com>
2019-08-22 15:47:46 +0200

    Separated searching and cloning. Added search fields year and max_repos
    Changed cache directory to the same cache directory as AnalyseProject. Cache file is deleted after cloning all.

--- CloneAllProjects.py
+++ CloneAllProjects.py
@@ -8,20 +8,23 @@ import requests, json, os, getpass
 demis = ['2018-project-560895-Linda','2018-project-571133-Kyle','2018-project-714227-Kayla','2018-project-717931-Kishan','2018-project-816424-Conrad','2018-project-837873-Malu']
 
 def main():
-    user, pswd = getInput()
-    cloneAllProjects(user, pswd)
+    user, pswd, year, max_repos = getInput()
+    cloneAllProjects(user, pswd, year, max_repos)
 
-def cloneAllProjects(user, pswd, access_token = '146da8c3ad930d598318a94068fe158c716f6496', directory = os.getcwd() + os.sep + r'Repositories'):
-    filename = os.getcwd() + os.sep + 'Repos.json'
+def getRepoNames(user, pswd, year = '2018', max_repos = '100', access_token = '146da8c3ad930d598318a94068fe158c716f6496'):
+    cacheDirectory = os.getcwd() +os.sep+r'.cache'
+    if not os.path.exists(cacheDirectory):
+        os.makedirs(cacheDirectory)
+    filename = cacheDirectory + os.sep + 'Repos-' + year + '-max=' + max_repos +'.json'
     if not os.path.exists(filename):
         headers = {
             'Accept': 'application/vnd.github.mercy-preview+json',
             'Content-type': 'application/json',
         }
         params = (
-            ('q', 'org:witseie-elen3009 2018-project-'),
-            ('access_token', ''),
-            ('per_page' , '100'),
+            ('q', 'org:witseie-elen3009 '+ year +'-project-'),
+            ('access_token', access_token),
+            ('per_page' , max_repos),
         )
         response = requests.get('https://api.github.com/search/repositories', headers=headers, params=params, auth=(user, pswd))
         json_string = response.json()['items']
@@ -30,16 +33,37 @@ def cloneAllProjects(user, pswd, access_token = '146da8c3ad930d598318a94068fe158
     else:
         with open(filename, 'r') as f:
             json_string = json.load(f)
-    
+    names = []
+    for repo in json_string:
+        name = repo['name']
+        if ((name not in demis) and ('Demi' not in name)):
+            names.append(name)
+    return names
+
+def cloneAllProjects(user, pswd, year = '2018', max_repos = '100', directory = os.getcwd() + os.sep + r'Repositories'):
+    repos = getRepoNames(user, pswd, year, max_repos)
+    repos = str(len(repos))
+    print(repos + ' repositories found.')
+    cacheDirectory = os.getcwd()+os.sep+r'.cache'
+    if not os.path.exists(cacheDirectory):
+        os.makedirs(cacheDirectory)
+    filename = cacheDirectory + os.sep + 'Repos-' + year + '-max=' + max_repos +'.json'
+    with open(filename, 'r') as f:
+        json_string = json.load(f)
     if not os.path.exists(directory):
         os.makedirs(directory)
     os.chdir(directory)    
     for repo in json_string:
-        name = repo['name']
-        if name not in demis:
-            print('Cloning repository: ' + name)
-            os.system('git clone --depth 1 ' + repo['ssh_url'])
+        cloneProject(repo)
     print('Done.')
+    os.chdir(cacheDirectory)
+    os.remove(filename)
+
+def cloneProject(repo):
+    name = repo['name']
+    if ((name not in demis) and ('Demi' not in name)):
+        print('Cloning repository: ' + name)
+        os.system('git clone --depth 1 ' + repo['ssh_url'])
 
 def getInput():
     while True:
@@ -48,7 +72,13 @@ def getInput():
     while True:
         pswd = getpass.getpass('Enter GitHub password: ')
         break
-    return user, pswd
+    while True:
+        year = input('Enter year to search for: ')
+        break
+    while True:
+        max_repos = input('Enter maximum number of repos to search for: ')
+        break
+    return user, pswd, year, max_repos
 
 if __name__ == '__main__':
     main()
